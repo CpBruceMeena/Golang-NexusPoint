9b9e55c82dc9b66cb10740d389ce77375341e64d
CpBruceMeena <sandeep10198@gmail.com>
2025-04-20 17:27:06 +0530

    Update generate script to output to current directory

--- proto/generate.sh
+++ proto/generate.sh
@@ -5,8 +5,8 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 # Generate code for golang-central
-protoc --go_out=../golang-central --go_opt=paths=source_relative \
-    --go-grpc_out=../golang-central --go-grpc_opt=paths=source_relative \
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     user.proto
 
 # Generate code for golang-grpc-app
